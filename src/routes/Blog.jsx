7d324dec9343a213abc4c39e43a1cc1d6898c7db
Charly <charly81.zirpoli@gmail.com>
2022-04-15 17:35:59 -0300

    Filter elements

--- src/routes/Blog.jsx
+++ src/routes/Blog.jsx
@@ -1,7 +1,12 @@
-import { Link } from "react-router-dom";
+import { useEffect } from "react";
+import { Link, useSearchParams } from "react-router-dom";
 import { useFetch } from "../hooks/useFetch";
 
 const Blog = () => {
+  let [searchParams, setSearchParams] = useSearchParams();
+
+  useEffect(() => {}, [searchParams]);
+
   const { data, error, loading } = useFetch(
     "https://jsonplaceholder.typicode.com/posts"
   );
@@ -14,16 +19,42 @@ const Blog = () => {
     return <h1>{error}</h1>;
   }
 
+  const handleChange = (e) => {
+    let filter = e.target.value;
+    if (filter) {
+      setSearchParams({ filter });
+    } else {
+      setSearchParams({});
+    }
+  };
+
   return (
     <div>
       <h1>Blog</h1>
-      {data.map((item) => (
-        <h4 key={item.id}>
-          <Link to={`/blog/${item.id}`}>
-            {item.id} - {item.title}
-          </Link>
-        </h4>
-      ))}
+      <input
+        type="text"
+        className="form-control mb-2"
+        value={searchParams.get("filter") || ""}
+        onChange={handleChange}
+      />
+
+      {data
+        .filter((item) => {
+          let filter = searchParams.get("filter");
+          if (!filter) {
+            return true;
+          } else {
+            let title = item.title.toLowerCase();
+            return title.startsWith(filter.toLowerCase());
+          }
+        })
+        .map((item) => (
+          <h4 key={item.id}>
+            <Link to={`/blog/${item.id}`}>
+              {item.id} - {item.title}
+            </Link>
+          </h4>
+        ))}
     </div>
   );
 };
